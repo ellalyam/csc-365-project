83821bb81e66acc7f0d88456083412fc7eb796c4
Ashley Navos <anavos@calpoly.edu>
2025-02-26 12:52:03 -0800

    push

--- src/main/java/com/example/project/SQLConnection.java
+++ src/main/java/com/example/project/SQLConnection.java
@@ -21,26 +21,6 @@ public class SQLConnection {
 
 
 
-    public static void main(String[] args) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connect = DriverManager.getConnection(
-                    "jdbc:mysql://ambari-node5.csc.calpoly.edu/anavos", "anavos", "029760100"); // Replace with database name (username), username, and password
-
-            Statement statement = connect.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM STUDENTS;");
-            while (rs.next()) {
-                String studentName = rs.getString(1); // name is first field
-                System.out.println("Student name = " +
-                        studentName);
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
 
 }
 
